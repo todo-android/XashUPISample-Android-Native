a72d2405b39ac24ffca45893563b6574e51736d7
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2021-04-04 22:38:31 +0530

    add device binding code

--- app/src/main/java/app/xash/upisample/MainActivity.kt
+++ app/src/main/java/app/xash/upisample/MainActivity.kt
@@ -2,19 +2,20 @@ package app.xash.upisample
 
 import android.annotation.SuppressLint
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.SubscriptionManager
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.icici.ultrasdk.AdaptersAndCallbacks.UltraSDKCallBack
+import com.icici.ultrasdk.ErrorCodes.ErrorCodes
+import com.icici.ultrasdk.ErrorCodes.RequestCodes
 import com.icici.ultrasdk.Models.USDKResponse
-import com.icici.ultrasdk.RequestModels.ListAccounsReq
-import com.icici.ultrasdk.RequestModels.PayToVirtualAccountReq
+import com.icici.ultrasdk.RequestModels.GetProfileIdReq
 import com.icici.ultrasdk.SDKManager
 import com.icici.ultrasdk.SDKMessageCallback
 
-class MainActivity : AppCompatActivity(), SDKMessageCallback {
+class MainActivity : AppCompatActivity(), SDKMessageCallback, UltraSDKCallBack {
     lateinit var sdkManager: SDKManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,38 +24,30 @@ class MainActivity : AppCompatActivity(), SDKMessageCallback {
     }
 
     @SuppressLint("MissingPermission")
-    fun initSDK () {
-        val subscriptionManager = getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
+    fun initSDK() {
+        val subscriptionManager =
+            getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
         val subInfo = subscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(0)
         val subId = subInfo.subscriptionId
         sdkManager = SDKManager.getSDKManager(
-                this,
-                "+918800233266", // from flutter
-                "com.xash.tech",
-                "84521654123456", // ANDROID_ID or IMEI, for now harcode
+            this,
+            "+917355074244", // from flutter
+            "com.xash.tech",
+            "84521654123456", // ANDROID_ID or IMEI, for now harcode
             subId.toString(),
-                "OPERATOR NAME", // can be null (can come from telephone)
-        false,
-                "XChange", //correct
-                "GLGOxLn2D8ARCBdyLHkRLMdtAPhLAwWI" // correct
+            "OPERATOR NAME", // can be null (can come from telephone)
+            false,
+            "XCHANGE", //correct
+            "GLGOxLn2D8ARCBdyLHkRLMdtAPhLAwWI" // correct
         )
         sdkManager.setSdkMessageCallback(this)
-        val cb = object: UltraSDKCallBack {
-            override fun onResponse(p0: USDKResponse?) {
-                Toast.makeText(this@MainActivity, "SUCCESS", Toast.LENGTH_SHORT).show()
-            }
 
-            override fun onFailure(p0: USDKResponse?, p1: Throwable?) {
-                Log.e("UPI", "FAIL", p1)
-                Toast.makeText(this@MainActivity, "FAIL", Toast.LENGTH_SHORT).show()
-            }
-
-        }
 
         sdkManager.invokeSDK(
             "XCH", // correct
-            "+919643339706", // VMN (providing soon)
-            cb)
+            "+919282123345", // VMN (providing soon)
+            this
+        )
 
         // TODO: sdkManager.payToVPA()
         // TODO: sdkManager.storeAccountDetails() - create account
@@ -66,6 +59,32 @@ class MainActivity : AppCompatActivity(), SDKMessageCallback {
 
     override fun toggleDeviceBindingBackGroundFail() {
         Log.w("UPI", "toggleDeviceBindingBackGroundFail")
+    }
+
+    override fun onResponse(response: USDKResponse) {
+        Log.e("RESPONSE", "$response")
+        Log.e("RESPONSE", "${RequestCodes.AUTHENTICATION.requestCode}")
+        if (response.reqCode.equals(RequestCodes.
+            AUTHENTICATION.requestCode)) {
+            if (response.response.equals(ErrorCodes.U200.errorCode)) {
+                //calling service to get profile Id
+                val req = GetProfileIdReq()
+                val reqCode = RequestCodes.GET_PROFILE_ID.requestCode
+                sdkManager.getProfileId(req, reqCode, this);
+                Toast.makeText(this, response.response, Toast.LENGTH_SHORT).show()
+            }
+        } else if (response.reqCode.equals(RequestCodes.SEND_SMS.requestCode, true)) {
+            if (response.response.equals("SMS Success", true)) {
+                Log.e("INTITATE", "$response")
+                sdkManager.requestForDeviceBinding()
+                Toast.makeText(this, response.response, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    override fun onFailure(p0: USDKResponse?, p1: Throwable?) {
+        Log.e("UPI", "FAIL :    ${p0?.message} ", p1)
+        Toast.makeText(this@MainActivity, "${p0?.message}", Toast.LENGTH_SHORT).show()
     }
 
 }
